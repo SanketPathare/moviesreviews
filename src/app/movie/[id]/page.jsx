4de6aa44cb4917cc65651e9420105fefe5178879
SanketPathare <sanketpathare8808@gmail.com>
2025-01-27 17:20:57 +0530

    11-commit

--- src/app/movie/[id]/page.jsx
+++ src/app/movie/[id]/page.jsx
@@ -1,5 +1,5 @@
-import AddToFav from '@/components/AddToFav';
-import Link from 'next/link';
+import AddToFav from "@/components/AddToFav";
+import Link from "next/link";
 
 export default async function MoviePage({ params }) {
   const { id: movieId } = await params;
@@ -10,13 +10,13 @@ export default async function MoviePage({ params }) {
 
   if (!res.ok) {
     return (
-      <div className='text-center mt-10'>
-        <h1 className='text-xl my-5'>
+      <div className="text-center mt-10">
+        <h1 className="text-xl my-5">
           Movie details are not available at the moment!
         </h1>
         {/* return home */}
         <p>
-          <Link href='/' className='hover:text-sky-600'>
+          <Link href="/" className="hover:text-sky-600">
             Go Home
           </Link>
         </p>
@@ -25,25 +25,25 @@ export default async function MoviePage({ params }) {
   }
 
   return (
-    <div className='w-full'>
-      <div className='p-4 md:pt-8 flex flex-col md:flex-row content-center max-w-6xl mx-auto md:space-x-6'>
+    <div className="w-full">
+      <div className="p-4 md:pt-8 flex flex-col md:flex-row content-center max-w-6xl mx-auto md:space-x-6">
         <img
           src={`https://image.tmdb.org/t/p/original/${
             movie.backdrop_path || movie.poster_path
           }`}
-          className='rounded-lg w-full md:w-96 h-56 object-cover'
+          className="rounded-lg w-full md:w-96 h-56 object-cover"
         ></img>
-        <div className='p-2'>
-          <h2 className='text-lg mb-3 font-bold'>
+        <div className="p-2">
+          <h2 className="text-lg mb-3 font-bold">
             {movie.title || movie.name}
           </h2>
-          <p className='text-lg mb-3'>{movie.overview}</p>
-          <p className='mb-3'>
-            <span className='font-semibold mr-1'>Date Released:</span>
+          <p className="text-lg mb-3">{movie.overview}</p>
+          <p className="mb-3">
+            <span className="font-semibold mr-1">Date Released:</span>
             {movie.release_date || movie.first_air_date}
           </p>
-          <p className='mb-3'>
-            <span className='font-semibold mr-1'>Rating:</span>
+          <p className="mb-3">
+            <span className="font-semibold mr-1">Rating:</span>
             {movie.vote_count}
           </p>
           <AddToFav
