3eb2cb48653b9a668086c11faeebacda95fc8c10
SanketPathare <sanketpathare8808@gmail.com>
2025-01-27 16:55:55 +0530

    5-commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import { FiThumbsUp } from 'react-icons/fi';
 export default function Card({ result }) {
   return (
-    <div className='group cursor-pointer sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-neutral-900 sm:m-2 transition-shadow duration-200'>
+    <div className='group cursor-pointer sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-neutral-800 sm:m-2 transition-shadow duration-200'>
       <Link href={`/movie/${result.id}`}>
         <img
           src={`https://image.tmdb.org/t/p/original/${
